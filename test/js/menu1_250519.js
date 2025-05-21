e5f283f97bfd9057fb640c729157368421779627
binseo <oppfgh0901@naver.com>
2025-05-21 09:19:41 +0900

    .

--- test/js/menu1_250519.js
+++ test/js/menu1_250519.js
@@ -68,7 +68,9 @@ $(document).ready(function(){
     $('header .tnb .search').on('focusin', function(){
         $('header').removeClass('menu_over')
         $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
-    })
+    }) // --> 접근성 때문에 Tab을 누르면 tnb까지가면 header가 오버 삭제
+
+
 
 
     /* 
