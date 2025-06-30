2caa70b8977f3ad3868c8993dcc90586f4a39ef8
binseo <oppfgh0901@naver.com>
2025-06-30 22:47:37 +0900

    250630

--- buyeo/js/main.js
+++ buyeo/js/main.js
@@ -159,29 +159,44 @@ $(document).ready(function(){
         slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
+            767: {    /* 640px 이상일때 적용 */
+                spaceBetween: 16,
+            },
             768: {    /* 640px 이상일때 적용 */
-                slidesPerView: 1,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
                 spaceBetween: 24,
             },
+
         },
         //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-        // autoplay: {  /* 팝업 자동 실행 */
-        //     delay: 2500,
-        //     disableOnInteraction: true,
-        // },
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 2500,
+            disableOnInteraction: true,
+        },
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.notice .popup .swiper .ctrl_wrap button.btn_next',
+            prevEl: '.notice .popup .swiper .ctrl_wrap button.btn_prev',
         },
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            el: '.notice .popup .swiper .ctrl_wrap .paging ', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+            // type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
         },
     });
-    // popup_swiper.autoplay.stop();  /* 일시정지 기능 */
-    // popup_swiper.autoplay.start();  /* 재생 기능 */
+
+    $('.notice .popup .swiper .ctrl_wrap button.btn_stop').on('click', function(){
+        // console.log('정지버튼 클릭')
+        popup_swiper.autoplay.stop();  /* 일시정지 기능 */
+        $(this).hide()
+        $('.notice .popup .swiper .ctrl_wrap button.btn_play').show()
+    })
+
+    $('.notice .popup .swiper .ctrl_wrap button.btn_play').on('click', function(){
+        // console.log('재생 버튼')
+        popup_swiper.autoplay.start();  /* 재생 기능 */
+        $(this).hide()
+        $('.notice .popup .swiper .ctrl_wrap button.btn_stop').show()
+    })
 
 
     /* ************************************** popup_swiper : 끝 ************************************ */
