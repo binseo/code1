9af073cbd0658d2614a8b3dd897186481ff81ce3
binseo <oppfgh0901@naver.com>
2025-07-23 12:40:51 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -34,6 +34,20 @@ $(document).ready(function(){
 		},
         
 	});// fullpage
+
+	$(window).on('scroll mousemove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
+		$('.cursor').css('left', e.pageX + 'px');
+		$('.cursor').css('top', e.pageY + 'px');
+	});
+	$('.project01 .group a').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+		$('.cursor').toggleClass('on');
+	});
+	$('.project02 .group a').hover(function(){ 
+		$('.cursor').toggleClass('on');
+	});
+	$('.project03 .group a').hover(function(){ 
+		$('.cursor').toggleClass('on');
+	});
 	
 }) //$(document).ready
 
